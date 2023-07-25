19dbd09b7d17b7fedb923c2b55ef958afc9a0beb
Aman Kumar <95896562+amannn007@users.noreply.github.com>
2023-07-25 23:17:33 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,40 @@
+const express=require('express')
+const morgan=require('morgan')
+const cors=require('cors')
+const bodyParser=require('body-parser')
+const colors=require('colors')
+const dotenv =require('dotenv')
+const connectDB = require('./config/db')
+
+//routes path
+const authRoute =require('./routes/authRoute')
+const errorHandler = require('./middlewares/errorMiddleware')
+
+//dotenv
+dotenv.config()
+
+//rest object
+const app=express();
+
+//mongo connection
+connectDB();
+
+//middlewares
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(morgan('dev'))
+app.use(errorHandler)
+
+const PORT =process.env.PORT || 8080
+
+
+//API routes
+app.use("/api/v1/auth",authRoute);
+app.use("/api/v1/openai",require("./routes/openaiRoutes"));
+
+
+//listen server
+app.listen(PORT,()=>{
+    console.log(`Server Running in ${process.env.DEV_MODE} mode on ${PORT}`.bgCyan.white);
+})
